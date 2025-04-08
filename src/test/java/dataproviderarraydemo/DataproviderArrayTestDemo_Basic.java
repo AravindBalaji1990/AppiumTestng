cf74fae60850dc26fffb8eefbd25481aa275ed8c
AravindBalaji1990 <aravindbalaji.balaji7@gmail.com>
2025-04-08 20:14:10 +0300

    new code testng - dataprovider

---
+++ src/test/java/dataproviderarraydemo/DataproviderArrayTestDemo_Basic.java
@@ -0,0 +1,63 @@
+package dataproviderarraydemo;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.remote.AutomationName;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.Duration;
+
+public class DataproviderArrayTestDemo_Basic {
+
+    AndroidDriver driver;
+
+    @BeforeMethod
+    public void start() throws IOException, InterruptedException {
+        System.out.println("this is the before");
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("Android");
+        options.setDeviceName("29221JEGR00379");
+        options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
+        options.setAppActivity("com.swaglabsmobileapp.MainActivity");
+        options.setAppPackage("com.swaglabsmobileapp");
+        options.setAppWaitForLaunch(true);
+        options.setAppWaitDuration(Duration.ofMillis(50000));
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+    }
+
+    @AfterMethod
+    public void teardown() throws InterruptedException {
+        Thread.sleep(5000);
+        System.out.println("this is the after");
+        driver.quit();
+
+    }
+
+    @DataProvider(name = "AuthenticationData")
+    public Object[][] credentials() {
+        return new Object[][]{{"standard_user", "secret_sauce"}, {"locked_out_user", "secret_sauce"}, {"problem_user", "secret_sauce"}};
+    }
+
+    @DataProvider(name = "authentication")
+    public Object[][] authData() {
+
+        return new Object[][]{{"standard_user", "secret_sauce"}};
+    }
+
+
+    @Test(dataProvider = "AuthenticationData")
+    public void testcase02(String sUsername, String sPassword) throws InterruptedException {
+        System.out.println("username from data provider : " + sUsername);
+        System.out.println("password from data provider: " + sPassword);
+        driver.findElement(AppiumBy.xpath("//android.widget.EditText[@content-desc=\"test-Username\"]")).click();
+        driver.findElement(AppiumBy.xpath("//android.widget.EditText[@content-desc=\"test-Username\"]")).sendKeys(sUsername);
+        driver.findElement(AppiumBy.xpath("//android.widget.EditText[@content-desc=\"test-Password\"]")).click();
+        driver.findElement(AppiumBy.xpath("//android.widget.EditText[@content-desc=\"test-Password\"]")).sendKeys(sPassword);
+    }
+}
--- src/test/java/parameterisationdemo/DemoTestParameterisation.java
+++ src/test/java/parameterisationdemo/DemoTestParameterisation.java
@@ -16,8 +16,8 @@ public class DemoTestParameterisation {
     AndroidDriver driver;
 
     @BeforeSuite
-    @Parameters({"packagename","activityname","devicename","inputdata"} )
-    public  void start(String packagename,String activityname, String devicename, String data) throws MalformedURLException, InterruptedException {
+    @Parameters({"packagename", "activityname", "devicename", "inputdata"})
+    public void start(String packagename, String activityname, String devicename, String data) throws MalformedURLException, InterruptedException {
         System.out.println("this is the before method");
         UiAutomator2Options options = new UiAutomator2Options();
         options.setPlatformName("Android");
@@ -27,13 +27,14 @@ public class DemoTestParameterisation {
         options.setAppActivity(activityname);
         options.setAppWaitForLaunch(true);
         options.setAppWaitDuration(Duration.ofMillis(50000));
-         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), options);
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), options);
         System.out.println("data from suite level" + data);
 
     }
 
     @AfterSuite
-    public  void teardown(){
+    public void teardown() throws InterruptedException {
+        Thread.sleep(5000);
         System.out.println("this is the after method");
         driver.quit();
 
@@ -41,26 +42,19 @@ public class DemoTestParameterisation {
 
 
     @Test
-    @Parameters({ "inputdata" })
+    @Parameters({"inputdata"})
     public void testcase1(String valuetiinput) throws InterruptedException {
-    System.out.println("this is the first case 1");
-    Thread.sleep(5000);
+        System.out.println("this is the first case 1");
+        Thread.sleep(5000);
         driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"Views\"]")).click();
         Thread.sleep(5000);
 //     WebElement item =  driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"TextFields\"]"));
         // this can only do a vertical scroll
         driver.findElement(new AppiumBy.ByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().text(\"TextFields\").instance(0))")).click();
         Thread.sleep(5000);
-        System.out.println("the value form xml : " +valuetiinput);
+        System.out.println("the value form xml : " + valuetiinput);
         driver.findElement(AppiumBy.xpath("//android.widget.EditText[@resource-id='io.appium.android.apis:id/edit1']")).click();
         driver.findElement(AppiumBy.xpath("//android.widget.EditText[@resource-id='io.appium.android.apis:id/edit1']")).sendKeys(valuetiinput);
 
     }
-
-//    @Test(priority = 1)
-//    public void testcase2(){
-//        System.out.println("this is the first case 2");
-//        System.out.println("to chec kthe attribute is displayed :" +driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"Accessiility\"]")).isDisplayed());
-//
-//    }
 }
